05a7405073353319f4cca30640008d7cc9e3fef0
Jack <zediious@aol.com>
2025-04-06 10:42:39 -0400

    file manager container length to 1K

--- resources/scripts/components/server/files/FileManagerContainer.tsx
+++ resources/scripts/components/server/files/FileManagerContainer.tsx
@@ -94,7 +94,7 @@ export default () => {
                     ) : (
                         <CSSTransition classNames={'fade'} timeout={150} appear in>
                             <div>
-                                {files.length > 250 && (
+                                {files.length > 1000 && (
                                     <div css={tw`rounded bg-yellow-400 mb-px p-3`}>
                                         <p css={tw`text-yellow-900 text-sm text-center`}>
                                             This directory is too large to display in the browser, limiting the output
